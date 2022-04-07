b544e2364a6c480f7f1c77c44ae9061e90c45fbd
Candy Fair <candice.bastien@gmail.com>
2022-04-07 16:53:50 +0200

    Restructure CSS folder

--- accordion.js
+++ accordion.js
@@ -1,8 +1,8 @@
-let accordion = document.getElementsByClassName("question");
+let question = document.getElementsByClassName("question");
 let i;
 
-for (i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
+for (i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     panel.style.maxHeight
@@ -10,3 +10,22 @@ for (i = 0; i < accordion.length; i++) {
       : panel.style.maxHeight = panel.scrollHeight + "px";
   });
 }
+
+
+
+// let acc = document.getElementsByClassName("question");
+
+// for (let i = 0; i < acc.length; i++) {
+//   acc[i].onclick = function() {
+//   	for (let i = 0; i < acc.length; i++) {
+//         acc[i].classList.remove('active');
+//         acc[i].style.maxHeight = null;
+//         acc[i].nextElementSibling.style.maxHeight = null;
+//     }
+//     this.style.maxHeight = this.scrollHeight+'px';
+//     this.classList.add('active');
+//     const panel = this.nextElementSibling;
+//     panel.style.maxHeight = panel.scrollHeight + 'px'
+    
+//   }
+// }
